c6afb4465c1a228c1086eb83f426d54894ef2e75
Patrick Justice <80479370+patrickj32@users.noreply.github.com>
2022-06-14 19:26:21 -0400

    progess with hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 
 import React, { Component } from 'react'
-// import Team from './PjComponets/Team'
-// import TeamTwoLogo from './PjComponets/images/Gonzo.jpg'
 import Game from './PjComponets/Game'
-// import GameWithHooks from './PjComponets/GameWithHooks'
+import GameWithHooks from './PjComponets/GameWithHooks'
 
 
 
@@ -15,9 +13,9 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Game />
         {/* <Game /> */}
-        {/* <GameWithHooks /> */}
+        <Game />
+        <GameWithHooks />
 
         
     
--- src/PjComponets/GameWithHooks.js
+++ src/PjComponets/GameWithHooks.js
@@ -1,66 +1,86 @@
-import { useEffect, useState, createRef} from 'react';
+import { useState } from 'react';
+// import Team from './Team'
+// import TeamTwoLogo from './images/Gonzo.jpg'
 
-function GameWithHooks(props){
-    
-    
-    
-    
-    let userInput = createRef()
+
+function GameWithHooks(props) {
+
+
+    let [shotstaken, setShotsTaken] = useState(0)
     let [score, setScore] = useState(0)
 
 
+    let increaseShotCounter = () => {
+        console.log("the button has been pressed")
 
-        let increaseShotCounter = () => {
-            console.log("yay")
-            this.setState(
-                (state, props) => ({
-                    score: state.score + 1
+        setShotsTaken(
 
-
-                })
-            )
-
-            var youScored = new Audio('./Audio/Swish.mp3');
-            youScored.play()
-
-
-        }
-        setScore(
-            (state, props) => ({
-                shotstaken: state.shotstaken + 1
-
-            })
+            shotstaken + 1
         )
 
+        if (Math.random() > .5) {
+
+            setScore(
+               score + 1
+            )
+        }
+    }
 
 
-}
+
+  
+
+
 
 
 
     let shotpercentage = 0
 
-    if (this.state.shotstaken > 0) {
-        shotpercentage = Math.round((this.state.score / this.state.shotstaken) * 100)
+    if (shotstaken > 0) {
+        shotpercentage = Math.round((score / shotstaken) * 100)
     }
 
 
-    // return (
-    //     <div>
-
-    //         <h1>The {this.props.teamname}</h1>
-    //         <img src={this.props.logo} alt="" className="logo" />
-    //         <h2>Shots taken:{this.state.shotstaken}</h2>
-    //         <h2 className='scorecounter'>Score:{this.state.score}</h2>
-    //         <h2>Shot Percentage:{shotpercentage} %</h2>
-    //         <button onClick={this.increaseShotCounter} className='shootbutton'>Shoot</button>
-
-    //     </div>
-
-    // )
 
 
-    
+
+    return (
+        <div className='GameWithHooks'>
+            <h1>**********With Hooks************</h1>
+
+            {/* <h1>The {this.props.teamname}</h1> */}
+            {/* <img src={this.props.logo} alt="" className="logo" /> */}
+            <h2>Shots taken:{shotstaken}</h2>
+            <h2 className='scorecounter'>Score:{score}</h2>
+            <h2>Shot Percentage:{shotpercentage} %</h2>
+            <button onClick={increaseShotCounter} className='shootbutton'>Shoot</button>
+
+
+
+            <div className="Game">
+                {/* <Team
+                    teamname="Tigers"
+                    logo="https://cdn.logojoy.com/wp-content/uploads/2018/05/30161552/8_big16-768x591.png"
+                />
+                <img src="https://www.nicepng.com/png/full/20-200345_street-fighter-vs-png-vs-street-fighter-logo.png"
+                    alt="" className='versusPic' /> */}
+
+                {/* <Team
+                    teamname="Gonzos"
+                    logo={TeamTwoLogo}
+                /> */}
+
+            </div >
+        </div >
+
+
+    )
+
+}
+
+
+
+
 
 
 export default GameWithHooks;
